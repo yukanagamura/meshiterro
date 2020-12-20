919d44c8d73e9d47896b403096c62aa045daa1ac
yukanagamura <iamyukaxxxx@gmail.com>
2020-12-20 05:43:35 +0000

    Initial Commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'f44612f3c38ff79ed40f1e1785262995ef46886d07eec2f0fec26208d530dabfd995896418b661c9503312c7a572297a2936b64a2e428b63a3fad66df684d954'
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+
+Rails.application.routes.draw do
+  get 'users/show'
+  devise_for :users
+  root 'post_images#index'
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+   resource :favorites, only: [:create, :destroy]
+   resources :post_comments, only: [:create, :destroy]
+  end
+   resources :users, only: [:show]
+   
+   resources :users, only: [:show, :edit, :update]
+end
